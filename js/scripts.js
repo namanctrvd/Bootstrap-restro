6adc618a62ea69b200a9d94cf7d8f26409afa0c1
Naman <mymail.naman@gmail.com>
2022-06-06 04:04:46 +0530

    cleaning code

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $("#mycarousel").carousel({ interval: 3000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
+    });
+
+    $("#loginBtn").click(function () {
+        $("#loginModal").modal();
+    });
+    $("#reserveBtn").click(function () {
+        $("#reserveModal").modal();
+    });
+});
